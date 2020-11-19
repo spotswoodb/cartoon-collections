9586bc1eea677e4ac4cfccc5ed5e0f4e263a7a3f
Ben Spotswood <spotswoodb@gmail.com>
2020-11-18 23:10:21 -0600

    commit tree 91e555d516befab184396e1037662543495df1f2

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves = []
+planeteer_calls = []
+find_the_cheese = []
+def roll_call_dwarves(dwarves)
+  i = 1
+  dwarves.each do |name|
+    puts "#{i}. #{name}"
+    i += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(hero)
+  hero.collect  {|phrase| phrase.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? do |call|
+    call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find {|name| cheese_types.include?(name)}
 end
